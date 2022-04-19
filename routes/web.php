c6d8eb14f2c94bdbbf306dea5bb1a2307920e002
edogalihr <edogalihr7@gmail.com>
2022-04-19 22:40:47 +0700

    edo

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\HistoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|-ravi------------------------------------------------------------------------
+|-ravi-edo-----------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
